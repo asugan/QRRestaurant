06234e4026710a3877733b7e58484fa829f0d9e2
asugan <cagatayeren1898@gmail.com>
2022-10-08 01:06:39 +0300

    Buttons

--- src/Components/AdminHome.jsx
+++ src/Components/AdminHome.jsx
@@ -11,9 +11,13 @@ function AdminHome({ isLoggedIn, user }) {
       <div className="admincontainer">
         <h1>Admin Paneli</h1>
         <h5>{user.email}</h5>
+        <AuthButtons />
         <div className="navbar">
           <Link className="navlink" to="/">
             Anasayfa
+          </Link>
+          <Link className="navlink" to="/admin/addcategory">
+            Kategori Ekle
           </Link>
           <Link className="navlink" to="/admin/post">
             Yemek Ekle
@@ -21,7 +25,6 @@ function AdminHome({ isLoggedIn, user }) {
           <Link className="navlink" to="/admin/masalar">
             Masalara Git
           </Link>
-          <AuthButtons />
         </div>
       </div>
     );
